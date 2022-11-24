d1edb0f9a7de0ce732de0823704ecf2a9b5c16f9
Edmund Nillas III <munilla333d@gmail.com>
2022-11-24 14:19:54 +0800

    Move entry point to Simulacra header file

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,8 +4,25 @@ class Application : public Simulacra
 {
 public:
     Application()
+        : vertices {
+             0.0f,  0.5f, 1.0f,
+             0.5f, -0.5f, 1.0f,
+            -0.5f, -0.5f, 1.0f
+        }
     {
         std::cout << "Application Object Created" << std::endl;
+
+        shader.ID = LoadShader("assets/shaders/vertex.vs", "assets/shaders/fragment.fs");
+
+        bufferLayout.VAO = CreateBufferLayout();
+        buffer.VBO = CreateBuffer();
+        BindBufferLayout(bufferLayout.VAO);
+        BindBuffer(buffer.VBO);
+        SetBuffer(vertices, sizeof(vertices));
+
+        SetBufferLayout(0, 3, 3 * sizeof(float), (void*)0);
+        EnableVAByIndex(0);
+
     }
 
     ~Application()
@@ -16,20 +33,22 @@ public:
     virtual void OnUpdate(float deltaTime) override
     {
         // std::cout << "Updating..." << std::endl;
+        UseShader(shader.ID);
+        BindBufferLayout(bufferLayout.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 
     virtual void OnEvent() override
     {
         // std::cout << "Polling Events..." << std::endl;
     }
+
+private:
+    float vertices[9];
+    Buffer buffer;
+    BufferLayout bufferLayout;
+    Shader shader;
 };
-
-
-int main(int argc, char** argv)
-{
-    RunApplication();
-    return 0;
-}
 
 Simulacra* CreateApplication()
 {
--- src/Simulacra.h
+++ src/Simulacra.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <array>
+#include <algorithm>
 
 #include "Core/Application.h"
 #include "Platform/OpenGL/GLBuffer.h"
 #include "Platform/OpenGL/GLBufferLayout.h"
 #include "Platform/OpenGL/GLShader.h"
 #include "Platform/OpenGL/GLTexture.h"
+
+int main(int argc, char** argv)
+{
+    RunApplication();
+    return 0;
+}
