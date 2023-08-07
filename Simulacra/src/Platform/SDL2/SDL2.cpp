7420696705dad896128c1f000e74041801ad73a9
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-08-08 07:25:27 +0800

    Add opengl context initialization

--- Simulacra/src/Platform/SDL2/SDL2.cpp
+++ Simulacra/src/Platform/SDL2/SDL2.cpp
@@ -1,11 +1,14 @@
 #include "SDL2.h"
 
+#include <glad/glad.h>
+
 namespace Simulacra 
 {
     struct SDLProps
     {
         SDL_Window* window = nullptr;
         SDL_Renderer* renderer = nullptr;
+        SDL_GLContext context;
     };
 
     static SDLProps s_SDLProps;
@@ -20,9 +23,20 @@ namespace Simulacra
             return false;
         }
 
+        SDL_GL_LoadLibrary(nullptr);
+
+        SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
         uint32_t windowFlags = 0;
         windowFlags |= SDL_WINDOW_SHOWN;
         windowFlags |= SDL_WINDOW_RESIZABLE;
+        windowFlags |= SDL_WINDOW_OPENGL;
         s_SDLProps.window = SDL_CreateWindow(
             title.c_str(),
             SDL_WINDOWPOS_UNDEFINED,
@@ -37,6 +51,11 @@ namespace Simulacra
             return false;
         }
 
+        s_SDLProps.context = SDL_GL_CreateContext(s_SDLProps.window);
+        SDL_GL_MakeCurrent(s_SDLProps.window, s_SDLProps.context);
+
+        gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress);
+
         uint32_t rendererFlags = 0;
         rendererFlags |= SDL_RENDERER_ACCELERATED;
         rendererFlags |= SDL_RENDERER_PRESENTVSYNC;
@@ -47,14 +66,20 @@ namespace Simulacra
             return false;
         }
 
+        SDL_GL_SetSwapInterval(1);
+
+        int w, h;
+        SDL_GetWindowSize(s_SDLProps.window, &w, &h);
+        glViewport(0, 0, w, h);
+
         return true;
     }
 
     void ClearSDLWindowBuffer()
     {
-        SDL_RenderClear(s_SDLProps.renderer);
-        SDL_SetRenderDrawColor(s_SDLProps.renderer, 125, 100, 75, SDL_ALPHA_OPAQUE);
-        SDL_RenderPresent(s_SDLProps.renderer);
+        glClearColor(0.6f, 1.0f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        SDL_GL_SwapWindow(s_SDLProps.window);
     }
 
     void PollSDLEvents(const std::function<void(Event)>& fn)
@@ -76,6 +101,7 @@ namespace Simulacra
 
     void ShutdownSDL()
     {
+        SDL_GL_DeleteContext(s_SDLProps.context);
         SDL_DestroyRenderer(s_SDLProps.renderer);
         SDL_DestroyWindow(s_SDLProps.window);
         SDL_Quit();
