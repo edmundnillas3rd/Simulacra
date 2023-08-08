7f8bbb6c1ea8704457d936199b42f5799b5c4f61
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-08-08 14:31:59 +0800

    Add rest of the logging levels

--- Simulacra/src/Log.h
+++ Simulacra/src/Log.h
@@ -6,12 +6,9 @@ namespace Simulacra
 {
     extern std::shared_ptr<spdlog::logger> globalLogger;
 
-    // TODO: Remove this function after implementing other logging macros
-    template<typename T>
-    void LogInfo(const std::string& format, T msg)
-    {
-        spdlog::info(format.c_str(), msg);
-    }
-
-    #define SIM_LOG_INFO(...) globalLogger->info(__VA_ARGS__);
+    #define SIM_LOG_INFO(...)           globalLogger->info(__VA_ARGS__)
+    #define SIM_LOG_DEBUG(...)          globalLogger->debug(__VA_ARGS__)
+    #define SIM_LOG_WARN(...)           globalLogger->warn(__VA_ARGS__)
+    #define SIM_LOG_CRITICAL(...)       globalLogger->critical(__VA_ARGS__)
+    #define SIM_LOG_ERROR(...)          globalLogger->error(__VA_ARGS__)
 }
