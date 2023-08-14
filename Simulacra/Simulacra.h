303769241fa70efed9f6f96d44a9b775c4f8ff18
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-08-14 12:54:09 +0800

    Refactor log to consider namespaces

--- Simulacra/Simulacra.h
+++ Simulacra/Simulacra.h
@@ -2,6 +2,7 @@
 #include "src/Application.h"
 #include "src/Layer.h"
 #include "src/Event.h"
+#include "src/Log.h"
 #include "src/Platform/Windows/FileSystem.h"
 
 // OpenGL Header files
--- Simulacra/src/Log.h
+++ Simulacra/src/Log.h
@@ -6,9 +6,9 @@ namespace Simulacra
 {
     extern std::shared_ptr<spdlog::logger> globalLogger;
 
-    #define SIM_LOG_INFO(...)           globalLogger->info(__VA_ARGS__)
-    #define SIM_LOG_DEBUG(...)          globalLogger->debug(__VA_ARGS__)
-    #define SIM_LOG_WARN(...)           globalLogger->warn(__VA_ARGS__)
-    #define SIM_LOG_CRITICAL(...)       globalLogger->critical(__VA_ARGS__)
-    #define SIM_LOG_ERROR(...)          globalLogger->error(__VA_ARGS__)
+    #define SIM_LOG_INFO(...)           ::Simulacra::globalLogger->info(__VA_ARGS__)
+    #define SIM_LOG_DEBUG(...)          ::Simulacra::globalLogger->debug(__VA_ARGS__)
+    #define SIM_LOG_WARN(...)           ::Simulacra::globalLogger->warn(__VA_ARGS__)
+    #define SIM_LOG_CRITICAL(...)       ::Simulacra::globalLogger->critical(__VA_ARGS__)
+    #define SIM_LOG_ERROR(...)          ::Simulacra::globalLogger->error(__VA_ARGS__)
 }
