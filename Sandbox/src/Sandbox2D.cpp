77c5db549ab08a2dc6f3b32cb1638ef5bbe6a00c
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-09-10 15:46:48 +0800

    Resolve Missing Dependencies

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -1,7 +1,5 @@
 #include "Sandbox2D.h"
 
-#include <math.h>
-
 Sandbox2D::Sandbox2D()
 {
     m_Shader = Simulacra::LoadShader({
@@ -10,22 +8,22 @@ Sandbox2D::Sandbox2D()
     });
 
     // Should be read in clockwise
-    float vertices[] = {
-        -1.0f, -1.0f, 0.0f, 1.0f,
-         1.0f, -1.0f, 0.0f, 1.0f,
-         1.0f,  1.0f, 0.0f, 1.0f,
-        -1.0f,  1.0f, 0.0f, 1.0f        
-    };
+    // float vertices[] = {
+    //     -1.0f, -1.0f, 0.0f, 1.0f,
+    //      1.0f, -1.0f, 0.0f, 1.0f,
+    //      1.0f,  1.0f, 0.0f, 1.0f,
+    //     -1.0f,  1.0f, 0.0f, 1.0f        
+    // };
 
     glCreateVertexArrays(1, &m_VAO);
-    glCreateBuffers(1, &m_VBO);
+    // glCreateBuffers(1, &m_VBO);
     glBindVertexArray(m_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), nullptr, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), nullptr, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    // glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
 
-    glEnableVertexAttribArray(0);
+    // glEnableVertexAttribArray(0);
 }
 
 Sandbox2D::~Sandbox2D()
@@ -35,8 +33,9 @@ Sandbox2D::~Sandbox2D()
 void Sandbox2D::OnUpdate(float deltaTime)
 {
     static float currentTime;
+    const float SPEED = 1.5f;
 
-    currentTime += 1.5f * deltaTime;
+    currentTime += SPEED * deltaTime;
     GLfloat attrib[] = { 
         (float)sin(currentTime) * 0.5f,
         (float)cos(currentTime) * 0.6f,
@@ -56,6 +55,7 @@ void Sandbox2D::OnUpdate(float deltaTime)
     glUseProgram(m_Shader.IDs[1]);
     glVertexAttrib4fv(0, attrib);
     glVertexAttrib4fv(1, attrib);
+    glBindVertexArray(m_VAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
