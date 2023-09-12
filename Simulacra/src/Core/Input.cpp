eed730a86912b2da4a7508cebf4c80ddf0dd6e98
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-09-12 14:56:07 +0800

    Add input system

---
+++ Simulacra/src/Core/Input.cpp
@@ -0,0 +1,13 @@
+#include "Input.h"
+
+#include <SDL.h>
+
+namespace Simulacra
+{
+    bool IsKeyPressed(VKEY key)
+    {
+        const Uint8* input = SDL_GetKeyboardState(nullptr);
+
+        return (bool)input[(int)key] == 1;
+    }   
+}
