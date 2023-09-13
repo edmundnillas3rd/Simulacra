5bf1d7c0bcf18e8676670009a8f911e581e90974
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-09-13 10:16:29 +0800

    Update API header file

--- Simulacra/Simulacra.h
+++ Simulacra/Simulacra.h
@@ -2,11 +2,15 @@
 #include "src/Core/Application.h"
 #include "src/Core/Layer.h"
 #include "src/Core/Event.h"
+#include "src/Core/Window.h"
 #include "src/Core/Log.h"
+#include "src/Core/Input.h"
 #include "src/Platform/Linux/FileSystem.h"
 
 // OpenGL Header files
 #include "src/Platform/OpenGL/Shader.h"
+#include "src/Platform/OpenGL/Framebuffer.h"
+#include "src/Camera.h"
 
 // ImGui Header files
 #include "imgui.h"
