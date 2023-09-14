ff1a2abfe80887691ccf8bb660f58020b5e771f8
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-09-14 18:16:39 +0800

    Update CMakeLists and API header file

--- Simulacra/Simulacra.h
+++ Simulacra/Simulacra.h
@@ -5,13 +5,14 @@
 #include "src/Core/Window.h"
 #include "src/Core/Log.h"
 #include "src/Core/Input.h"
-#include "src/Platform/Linux/FileSystem.h"
+#include "src/Camera.h"
+#include "src/Scene.h"
 
 // OpenGL Header files
 #include "src/Platform/OpenGL/Shader.h"
 #include "src/Platform/OpenGL/Framebuffer.h"
 #include "src/Platform/OpenGL/Texture.h"
-#include "src/Camera.h"
+#include "src/Platform/Linux/FileSystem.h"
 
 // ImGui Header files
 #include "imgui.h"
