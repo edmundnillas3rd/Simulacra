df80555c886f5fe6944dba2b8a0ef2c310c88a25
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-03-31 09:48:05 +0800

    Remove the main tentative imgui layout

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -11,22 +11,5 @@ public:
     virtual void OnUpdate(float deltaTime) override;
     virtual void OnImGuiRender() override;
     virtual void OnEvent(Simulacra::Event event) override;
-private:
-    Simulacra::Shader m_Shader;
-    Simulacra::Framebuffer m_FBO;
-    Simulacra::Camera m_Camera;
-    Simulacra::Texture m_Texture;
     
-    GLuint m_CubeVAO;
-    GLuint m_CubeVBO;
-
-    GLuint m_ColorBuffer;
-    GLuint m_RBO;
-    GLuint m_QuadVAO;
-    GLuint m_QuadVBO;
-
-    bool m_FreeRoam;
-    bool firstMouse = true;
-    float lastX;
-    float lastY;
 };
