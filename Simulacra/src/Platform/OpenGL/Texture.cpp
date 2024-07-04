07dd91b890de7df3414f5d0b4c553ccabe23c4ac
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-07-04 12:16:06 +0800

    Add a texture loader

--- Simulacra/src/Platform/OpenGL/Texture.cpp
+++ Simulacra/src/Platform/OpenGL/Texture.cpp
@@ -1,17 +1,32 @@
 #include "Texture.h"
 
+#include <stb_image.h>
+
 namespace Simulacra
 {
-    Texture LoadTexture()
+    Texture LoadTexture(const std::string& path)
     {
         Texture texture;
 
+        int n = 0;
+        int forceChannels = 4;
+        unsigned char* image_data = stbi_load(path.c_str(), &texture.Texels.x, &texture.Texels.y, &n, forceChannels);
+        if (!image_data)
+        {
+            std::cout << "Error: Could not load " + path << std::endl;
+        }
+
+        stbi_set_flip_vertically_on_load(1);
+
         glCreateBuffers(1, &texture.Data);
+        glActiveTexture(GL_TEXTURE0);
         BindTexture(texture.Data);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.Texels.x, texture.Texels.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
         return texture;
     }
--- Simulacra/src/Platform/OpenGL/Texture.h
+++ Simulacra/src/Platform/OpenGL/Texture.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <string>
+#include <iostream>
+
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 namespace Simulacra
 {
     struct Texture
     {
+        glm::ivec2 Texels;
         GLuint Data;
     };
 
-    Texture LoadTexture();
+    Texture LoadTexture(const std::string& path);
     void BindTexture(GLuint texture = 0);
 }
