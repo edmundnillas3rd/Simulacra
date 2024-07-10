31f09a35a22a37635c10fe0447f0e641d6cb0903
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-07-10 12:32:03 +0800

    Refactor to include free image data after being loaded

--- Simulacra/src/Platform/OpenGL/Texture.cpp
+++ Simulacra/src/Platform/OpenGL/Texture.cpp
@@ -21,13 +21,15 @@ namespace Simulacra
 
         glCreateBuffers(1, &texture.TextureID);
         glActiveTexture(GL_TEXTURE0);
-        BindTexture(texture.TextureID);
+        glBindTexture(GL_TEXTURE_2D, texture.TextureID);
 
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.Size.x, texture.Size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(imageData);
 
         return texture;
     }
