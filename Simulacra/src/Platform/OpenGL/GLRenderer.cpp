2172c898e1821df923507f7327905344cf18bf6b
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-08-09 19:53:56 +0800

    Add render computer shader call

--- Simulacra/src/Platform/OpenGL/GLRenderer.cpp
+++ Simulacra/src/Platform/OpenGL/GLRenderer.cpp
@@ -5,6 +5,12 @@
 
 namespace Simulacra
 {
+    void RenderComputeShader(uint32_t width, uint32_t height)
+    {
+        glDispatchCompute((unsigned int)width, (unsigned int)height, 1);
+        glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+    }
+
     void DrawIndices(uint32_t count)
     {
         glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, 0);
--- Simulacra/src/Platform/OpenGL/GLRenderer.h
+++ Simulacra/src/Platform/OpenGL/GLRenderer.h
@@ -6,5 +6,6 @@
 
 namespace Simulacra
 {
+    void RenderComputeShader(uint32_t width, uint32_t height);
     void DrawIndices(uint32_t count);
 }
