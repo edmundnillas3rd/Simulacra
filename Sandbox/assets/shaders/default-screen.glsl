de8fe5f8b73c14a2e51c6bc28a24310e70dee1b6
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-08-10 14:04:33 +0800

    Add description for quad shader to avoid confusion

--- Sandbox/assets/shaders/default-screen.glsl
+++ Sandbox/assets/shaders/default-screen.glsl
@@ -1,27 +1,29 @@
 #shader vertex
 #version 460 core
+// This is the default screen to render for framebuffers
 
-layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec2 aTex;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec4 aColor;
+layout (location = 2) in vec2 aTex;
 
-out vec2 OutTex;
+out vec2 TexCoords;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
-    OutTex = aTex;
+    gl_Position = vec4(aPos, 1.0);
+    TexCoords = aTex;
 }
 
 #shader fragment
 #version 460 core
-
-in vec2 OutTex;
-
 out vec4 color;
+
+in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
 void main()
 {
-    color = texture(screenTexture, OutTex);
+    vec3 texCol = texture(screenTexture, TexCoords).rgb;
+    color = vec4(texCol, 1.0);
 } 
