019c6afee1b7922ed559237477a24f8d3ce4bf19
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-08-16 14:23:06 +0800

    Add framebuffer blitting and refactor framebuffer basic function

--- Simulacra/src/Platform/OpenGL/GLFramebuffer.h
+++ Simulacra/src/Platform/OpenGL/GLFramebuffer.h
@@ -9,14 +9,22 @@ namespace Simulacra
     struct Framebuffer
     {
         uint32_t ID;
-        std::string Name;
-        Texture TextureBuffer;
-        VertexArrayBuffer FramebufferVAO;
+        Texture AttachColorTexture;
     };
 
-    Framebuffer CreateFramebuffer(const std::string& name);
-    void BindFramebuffer(uint32_t id);
+    Framebuffer CreateFramebuffer(uint32_t width, uint32_t height);
+    void BindFramebuffer(const Framebuffer& framebuffer);
+    void UnbindFramebuffer();
+    void WriteFramebuffer(const Framebuffer& framebuffer);
+    void ReadFramebuffer(const Framebuffer& framebuffer);
+    void WriteToDefaultFramebuffer();
+    void ReadToDefaultFramebufferFramebuffer();
     void SetViewport(uint32_t width, uint32_t height);
+    void BlitFramebuffer(const Framebuffer& srcFramebuffer, const Framebuffer& destFramebuffer);
+    void BlitFramebuffer(const Framebuffer &srcFramebuffer, uint32_t width, uint32_t height);
+
+    void SetColor(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
+    void Clear();
 
     Texture CreateTextureAttachment(uint32_t width, uint32_t height);
 
