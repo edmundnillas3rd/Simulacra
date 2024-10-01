a80df0730a7ae615f2619f3b763091993c2ef672
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-10-01 13:47:09 +0800

    (dev-refactor) Add update window module

--- Simulacra/src/Window.h
+++ Simulacra/src/Window.h
@@ -15,5 +15,6 @@ namespace Simulacra
 
     // NOTE(Edmund): Interface for platform implementation
     void CreatePlatformWindow(const char* title, uint32_t width, uint32_t height);
+    void UpdateWindow();
     void DestroyPlatformWindow();
 }
