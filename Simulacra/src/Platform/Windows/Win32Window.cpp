d828e8deba7d053532fd83fa03becbf4d6699eae
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-10-26 14:43:34 +0800

    (dev-refactor) Update Window clear and swap buffer sequence

--- Simulacra/src/Platform/Windows/Win32Window.cpp
+++ Simulacra/src/Platform/Windows/Win32Window.cpp
@@ -130,8 +130,8 @@ namespace Simulacra
 
     void UpdateWindow()
     {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         SDL_GL_SwapWindow(s_Window);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
     void DestroyPlatformWindow()
