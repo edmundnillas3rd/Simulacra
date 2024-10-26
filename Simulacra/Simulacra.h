312e24225cb8c40a9766e9fea5e0620b98f78f4c
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-10-26 14:42:27 +0800

    (dev-refactor) Update Simulacra header files

--- Simulacra/Simulacra.h
+++ Simulacra/Simulacra.h
@@ -2,3 +2,8 @@
 
 #include "src/Application.h"
 #include "src/Logger.h"
+#include "src/Renderer/Renderer2D.h"
+
+#include "src/Platform/OpenGL/OpenGLTexture.h";
+#include "src/Platform/OpenGL/OpenGLShader.h";
+#include "src/Platform/OpenGL/OpenGLRenderer.h";
