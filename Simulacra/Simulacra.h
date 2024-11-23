8c7546623cea0dd6106e7ed224728a4999d6543d
Edmund Nillas III <edmundnillas3rd@gmail.com>
2024-11-23 17:58:53 +0800

    Add event header files

--- Simulacra/Simulacra.h
+++ Simulacra/Simulacra.h
@@ -3,9 +3,13 @@
 #include "src/Application.h"
 #include "src/Logger.h"
 
+#include "src/Events/Event.h"
+#include "src/Events/MouseEvents.h"
+#include "src/Events/KeyEvents.h"
+
 #include "src/Renderer/OrthographicCamera.h"
 #include "src/Renderer/Renderer2D.h"
 
-#include "src/Platform/OpenGL/OpenGLTexture.h";
-#include "src/Platform/OpenGL/OpenGLShader.h";
-#include "src/Platform/OpenGL/OpenGLRenderer.h";
+#include "src/Platform/OpenGL/OpenGLTexture.h"
+#include "src/Platform/OpenGL/OpenGLShader.h"
+#include "src/Platform/OpenGL/OpenGLRenderer.h"
