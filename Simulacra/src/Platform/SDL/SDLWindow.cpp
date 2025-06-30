b8b77eba71ef293bb8cb332774edb8d140263c0f
Edmund Nillas III <edmundnillas3rd@gmail.com>
2025-07-01 07:45:25 +0800

    Move opengl debug callback to lambda function for brevity

--- Simulacra/src/Platform/SDL/SDLWindow.cpp
+++ Simulacra/src/Platform/SDL/SDLWindow.cpp
@@ -14,7 +14,7 @@
 #include "../../Events/KeyEvents.h"
 #include "../../Events/MouseEvents.h"
 
-#include "../OpenGL/OpenGLDebug.h"
+#include "../PlatformUtils.h"
 
 namespace Simulacra
 {
@@ -66,7 +66,44 @@ namespace Simulacra
 
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageCallback([](GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *userParam) {
+            if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+            ConsoleLog("ID: {}", id);
+            ConsoleLog("Message: {}", message);
+
+            switch (source)
+            {
+                case GL_DEBUG_SOURCE_API:                               ConsoleLog("Source: API"); break;
+                case GL_DEBUG_SOURCE_WINDOW_SYSTEM:                     ConsoleLog("Source: Window System"); break;
+                case GL_DEBUG_SOURCE_SHADER_COMPILER:                   ConsoleLog("Source: Shader Compiler"); break;
+                case GL_DEBUG_SOURCE_THIRD_PARTY:                       ConsoleLog("Source: Third Party"); break;
+                case GL_DEBUG_SOURCE_APPLICATION:                       ConsoleLog("Source: Application"); break;
+                case GL_DEBUG_SOURCE_OTHER:                             ConsoleLog("Source: Other"); break;
+            }
+
+            switch (type)
+            {
+                case GL_DEBUG_TYPE_ERROR:                               ConsoleLog("Type: Error"); break;
+                case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:                 ConsoleLog("Type: Deprecated Behavior"); break;
+                case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:                  ConsoleLog("Type: Undefined Behavior"); break;
+                case GL_DEBUG_TYPE_PORTABILITY:                         ConsoleLog("Type: Portability"); break;
+                case GL_DEBUG_TYPE_PERFORMANCE:                         ConsoleLog("Type: Performance"); break;
+                case GL_DEBUG_TYPE_MARKER:                              ConsoleLog("Type: Marker"); break;
+                case GL_DEBUG_TYPE_PUSH_GROUP:                          ConsoleLog("Type: Push Group"); break;
+                case GL_DEBUG_TYPE_POP_GROUP:                           ConsoleLog("Type: Pop Group"); break;
+                case GL_DEBUG_TYPE_OTHER:                               ConsoleLog("Type: Other"); break;
+            }
+
+            switch (severity)
+            {
+                case GL_DEBUG_SEVERITY_HIGH:                            ConsoleLog("Severity: High"); break;
+                case GL_DEBUG_SEVERITY_MEDIUM:                          ConsoleLog("Severity: Medium"); break;
+                case GL_DEBUG_SEVERITY_LOW:                             ConsoleLog("Severity: Low"); break;
+                case GL_DEBUG_SEVERITY_NOTIFICATION:                    ConsoleLog("Severity: Notification"); break;
+            }
+        }, nullptr);
+    
         glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
         glViewport(0, 0, props.Width, props.Height);
     
