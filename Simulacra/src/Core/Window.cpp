24ba6783bd2c17e9855f8012d542a64506cefc8a
Edmund Nillas III <edmundnillas3rd@gmail.com>
2025-07-15 19:01:04 +0800

    Remove platform dependent macros

--- Simulacra/src/Core/Window.cpp
+++ Simulacra/src/Core/Window.cpp
@@ -7,9 +7,7 @@ namespace Simulacra
 {
     void StartWindowSubsystem(const WindowProps& props)
     {
-    #ifdef _WIN32
         CreatePlatformWindow(props);
-    #endif
     }
 
     void ShutdownWindowSubsystem()
