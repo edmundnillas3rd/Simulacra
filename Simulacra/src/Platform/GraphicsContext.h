84c78a119c12344c99a4f8b00f37f388c0a4a1cf
Edmund Nillas III <edmundnillas3rd@gmail.com>
2025-08-13 07:28:09 +0800

    Remove unused header file for graphics context

--- Simulacra/src/Platform/GraphicsContext.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Simulacra
-{
-    using GraphicsContext = void*;
-
-    void* CreateGraphicsContext(void* window);
-    void MakeContextCurrent(void* window, GraphicsContext context);
-}
