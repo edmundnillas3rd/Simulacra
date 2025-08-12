b8e50fc72b7cd15175a3cfe0b5e82c58d009fa44
Edmund Nillas III <edmundnillas3rd@gmail.com>
2025-08-12 15:45:04 +0800

    Update project vendors to use the conan package manager

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,27 @@
 import os
 
-from conan import ConanFile # type: ignore
-from conan.errors import ConanInvalidConfiguration # type: ignore
-from conan.tools.cmake import cmake_layout # type: ignore
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
+    project_engine_name = "Simulacra"
+    project_vendor_dir = "vendor"
+
     def requirements(self):
         self.requires("sdl/2.24.0")
         self.requires("spdlog/1.12.0")
         self.requires("glm/cci.20230113")
+        self.requires("imgui/1.90.5-docking")
+
+    def generate(self):
+        copy(self, "*sdl*", os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"), os.path.join(self.source_folder, self.project_engine_name, self.project_vendor_dir, "imgui_bindings"))
+        copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"), os.path.join(self.source_folder, self.project_engine_name, self.project_vendor_dir, "imgui_bindings"))
+        copy(self, "*imgui_demo.cpp*", os.path.join(self.dependencies["imgui"].package_folder, "res", "src"), os.path.join(self.source_folder, self.project_engine_name, self.project_vendor_dir, "imgui_bindings"))
 
     def configure(self):
         self.options["glad"].gl_profile = "core"
