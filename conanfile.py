efb315bdedcd966febd08f1e62f9fc7ba0c80d74
Edmund Nillas III <edmundnillasiii@gmail.com>
2025-08-20 19:12:32 +0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -23,17 +23,5 @@ class SimulacraDepsRecipe(ConanFile):
         copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"), os.path.join(self.source_folder, self.project_engine_name, self.project_vendor_dir, "imgui_bindings"))
         copy(self, "*imgui_demo.cpp*", os.path.join(self.dependencies["imgui"].package_folder, "res", "src"), os.path.join(self.source_folder, self.project_engine_name, self.project_vendor_dir, "imgui_bindings"))
 
-    def configure(self):
-        self.options["glad"].gl_profile = "core"
-        self.options["glad"].gl_version = "4.6"
-
-    def build_requirements(self):
-        if self.settings.os != "Windows":
-            self.tool_requires("cmake/3.26")
-
     def layout(self):
         cmake_layout(self)
-
-    def validate(self):
-        if self.settings.os != "Windows":
-            raise ConanInvalidConfiguration(f"{self.settings.os} is not supported")
